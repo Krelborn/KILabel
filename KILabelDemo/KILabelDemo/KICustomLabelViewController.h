4a36aecedf4526ed16a9723715f597f63ecdea3e
Matt Styles <matt@compiledcreations.com>
2015-05-11 11:33:22 +0100

    Demo to show custom classifiers.

---
+++ KILabelDemo/KILabelDemo/KICustomLabelViewController.h
@@ -0,0 +1,17 @@
+//
+//  KICustomLabelViewController.h
+//  KILabelDemo
+//
+//  Created by Matt Styles on 11/05/2015.
+//  Copyright (c) 2015 Matthew Styles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class KILabel;
+
+@interface KICustomLabelViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet KILabel *label;
+
+@end
